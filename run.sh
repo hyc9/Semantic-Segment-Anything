e06c7700d4e5fe3746de8b6bf18482226196c067
hyc9 <chengyu@mail.ustc.edu.cn>
2025-06-11 13:13:15 +0000

    lc

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
+cd /chengyu/MLLM/Semantic-Segment-Anything;
+git pull;
 source activate ssa;
-python /chengyu/MLLM/Semantic-Segment-Anything/scripts/semantic_mask_pipeline_lc.py --world_size 2
+python scripts/semantic_mask_pipeline_lc.py --world_size 2
